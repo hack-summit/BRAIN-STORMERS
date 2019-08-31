c159c8dba7b086784f1ee9f3ffff9a0416b4d36e
KMukilanRaja <54588558+KMukilanRaja@users.noreply.github.com>
2019-08-31 19:15:28 +0530

    Add files via upload

---
+++ projectt.py
@@ -0,0 +1,118 @@
+#servo code
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+SERVO = 14
+GPIO.setup(SERVO,GPIO.OUT)
+
+pwm=GPIO.PWM(SERVO,100)
+pwm.start(5)
+
+angle1=10
+duty1=float(angle1)/10 + 2.5
+
+angle2=90
+duty2=float(angle2)/10 + 2.5
+
+ck=0
+while ck<=0:
+	pwm.ChangeDutyCycle(duty1)
+	time.sleep(0.8)
+	pwm.ChangeDutyCycle(duty2)
+	time.sleep(0.8)
+	ck=ck+1
+time.sleep(1)
+GPIO.cleanup()	
+#stepper motor coded
+AA = 15
+BB = 18
+CC = 23
+DD = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(AA, GPIO.OUT)
+GPIO.setup(BB, GPIO.OUT)
+GPIO.setup(CC, GPIO.OUT)
+GPIO.setup(DD, GPIO.OUT)
+for i in range(128):
+          GPIO.output(DD,GPIO.HIGH)        
+	  time.sleep(0.005)
+	  GPIO.output(DD,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.HIGH)
+	  GPIO.output(DD,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.LOW)
+	  GPIO.output(DD,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.HIGH)
+	  GPIO.output(CC,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.LOW)
+	  GPIO.output(CC,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.HIGH)
+	  GPIO.output(BB,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.LOW)
+	  GPIO.output(BB,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.HIGH)
+	  GPIO.output(DD,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.LOW)
+	  GPIO.output(DD,GPIO.LOW)
+          time.sleep(0.005)
+time.sleep(2)	  
+for i in range(128):
+          GPIO.output(AA,GPIO.HIGH)
+	  GPIO.output(DD,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.LOW)
+	  GPIO.output(DD,GPIO.LOW)
+          time.sleep(0.005)
+	  GPIO.output(AA,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.HIGH)
+	  GPIO.output(BB,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(AA,GPIO.LOW)
+	  GPIO.output(BB,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.HIGH)
+	  GPIO.output(CC,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(BB,GPIO.LOW)
+	  GPIO.output(CC,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.HIGH)
+	  GPIO.output(DD,GPIO.HIGH)
+	  time.sleep(0.005)
+	  GPIO.output(CC,GPIO.LOW)
+	  GPIO.output(DD,GPIO.LOW)
+	  time.sleep(0.005)
+	  GPIO.output(DD,GPIO.HIGH)        
+	  time.sleep(0.005)
+	  GPIO.output(DD,GPIO.LOW)
+	  time.sleep(0.005)
